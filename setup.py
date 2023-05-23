b7297988136a18d80a17e0e4f4e46733b83fbdcd
nesnoj <jonathan.amme@rl-institut.de>
2023-05-23 17:04:43 +0200

    Restrict pandas to <=1.5.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages= ['disaggregator'],
     include_package_data=True,
     install_requires=['numpy',
-                      'pandas',
+                      'pandas<=1.5.3',
                       'tables',
                       'pyyaml',
                       'requests',
